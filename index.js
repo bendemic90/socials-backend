bc5d00a5ad90786fc315d70aa581bcf05510bef0
Ben T <ben.trevethan@gmail.com>
2021-05-25 19:35:42 +0930

    initialising back end for heroku

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js'
+
+const app = express()
+dotenv.config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+app.use(cors())
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send(`Welcome to bc-proj API`)
+})
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false);
